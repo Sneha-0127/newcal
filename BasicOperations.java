ae825bc6bc27d9b85299fecf815cf71eeeee133a
Sneha-0127 <snehamk27112005@gmail.com>
2025-05-06 10:01:57 +0530

    java

---
+++ BasicOperations.java
@@ -0,0 +1,14 @@
+
+public class BasicOperations {
+
+    public static void main(String[] args){
+        int a=12;
+        int b=27;
+        int sum = a+b;
+        int diff=a-b;
+        int prod=a*b;
+        System.out.println("Addition"+sum);
+        System.out.println("Subtraction"+diff);
+        System.out.println("multiplication"+prod);
+    }
+}
